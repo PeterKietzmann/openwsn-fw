a6cbe1bf07a7dcbb6fabe5b3ff5c253cccde8e81
Peter Kietzmann <peter.kietzmann@haw-hamburg.de>
2017-07-25 22:34:53 +0200

    change from periph_timer to rtt in sctimer adaption

--- bsp/boards/riot-adaption/sctimer.c
+++ bsp/boards/riot-adaption/sctimer.c
@@ -5,15 +5,17 @@
 #include "board_info.h"
 #include "sctimer.h"
 
-#include "periph/timer.h"
+#include "periph/rtt.h"
 
-#define ENABLE_DEBUG    (1)
+#define ENABLE_DEBUG    (0)
 #include "debug.h"
 
-// TODO this is much too hardware dependant
+// TODO this is toooo hardware dependant
 // ========================== define ==========================================
-#define TIMERLOOP_THRESHOLD          0xffffff     // 511 seconds @ 32768Hz clock
-#define OVERFLOW_THRESHOLD           0x7fffffff   // as openmotestm32 uses 16kHz, the upper timer overflows when timer research to 0x7fffffff
+// 511 seconds @ 32768Hz clock
+#define TIMERLOOP_THRESHOLD          0xffffff
+// as 16kHz is used, the upper timer overflows when timer research to 0x7fffffff
+#define OVERFLOW_THRESHOLD           0x7fffffff
 #define MINIMUM_COMPAREVALE_ADVANCE  10
 
 // ========================== variable ========================================
@@ -26,7 +28,7 @@ typedef struct {
 
 sctimer_vars_t sctimer_vars;
 
-static void sctimer_isr_internal(void *arg, int chan);
+static void sctimer_isr_internal(void *arg);
 
 // ========================== protocol =========================================
 
@@ -36,10 +38,7 @@ static void sctimer_isr_internal(void *arg, int chan);
 void sctimer_init(void){
     DEBUG("sctimer_init\n");
     memset(&sctimer_vars, 0, sizeof(sctimer_vars_t));
-    // TODO introduce macro. Question: Should we use RTC here?
-    if (timer_init(TIMER_DEV(1), 16000, sctimer_isr_internal, NULL) < 0) {
-        DEBUG("sctimer_init: ERROR on initialization\n");
-    }
+    rtt_init();
 }
 
 void sctimer_set_callback(sctimer_cbt cb){
@@ -83,7 +82,7 @@ void sctimer_setCompare(uint32_t val){
     if (val > TIMERLOOP_THRESHOLD && val < 2*TIMERLOOP_THRESHOLD ){
         sctimer_vars.convertUnlock = true;
     }
-    
+
     // update value to be compared according to timer condition
     if (val <= OVERFLOW_THRESHOLD){
         if (sctimer_vars.convert){
@@ -100,18 +99,18 @@ void sctimer_setCompare(uint32_t val){
             val |= 0x80000000;
         }
     }
-
     DISABLE_INTERRUPTS();
-    if (timer_read(TIMER_DEV(1)) - val < TIMERLOOP_THRESHOLD){
-        // the timer is already late, schedule the ISR right now manually 
-        timer_set(TIMER_DEV(1), 0, 0);
+    if (rtt_get_counter() - val < TIMERLOOP_THRESHOLD){
+        // the timer is already late, schedule the ISR cb function manually
+        // It is not executed in interrupt context but won't be
+        // interrupted as interrupts were disabled
+        sctimer_isr_internal(NULL);
     } else {
-        if (val-timer_read(TIMER_DEV(1))<MINIMUM_COMPAREVALE_ADVANCE){
-            // schedule ISR right now manually
-            timer_set(TIMER_DEV(1), 0, 0);
+        if (val-rtt_get_counter()<MINIMUM_COMPAREVALE_ADVANCE){
+            sctimer_isr_internal(NULL);
         } else {
             // schedule the timer at val
-            timer_set(TIMER_DEV(1), 0, (unsigned int)val);
+            rtt_set_alarm(val, sctimer_isr_internal, NULL);
         }
     }
 
@@ -124,46 +123,32 @@ void sctimer_setCompare(uint32_t val){
  \returns The current value of the timer's counter.
 */
 uint32_t sctimer_readCounter(void){
-    DEBUG("sctimer_readCounter:%i\n", (int)timer_read(TIMER_DEV(1)));
-    // TODO overflow handling? xtimer?
-    return (uint32_t)timer_read(TIMER_DEV(1));
+    uint32_t counter;
+    counter = rtt_get_counter();
+    counter = counter & OVERFLOW_THRESHOLD;
+    counter = counter << 1;
+    DEBUG("sctimer_readCounter:%" PRIu32 "\n", counter);
+    return counter;
 }
 
 void sctimer_enable(void){
     DEBUG("sctimer_enable\n");
-    timer_start(TIMER_DEV(1));
+    rtt_poweron();
 }
 
 void sctimer_disable(void){
     DEBUG("sctimer_disable\n");
-    timer_stop(TIMER_DEV(1));
+    rtt_poweroff();
 }
 
 // ========================== private =========================================
 
-void sctimer_isr_internal(void *arg, int chan){
+// Do I need to "kick" the scheduler?
+void sctimer_isr_internal(void *arg){
     (void)arg;
-    (void)chan;
     DEBUG("sctimer_isr_internal\n");
-    // debugpins_isr_set(); // for debugging
-
+    // debugpins_isr_set();
     if (sctimer_vars.sctimer_cb!=NULL) {
         sctimer_vars.sctimer_cb();
     }
-
-    // debugpins_isr_clr(); // for debugging
 }
-
-//=========================== interrupt handlers ==============================
-
-/*kick_scheduler_t sctimer_isr() {
-    if (sctimer_vars.sctimer_cb!=NULL) {
-        
-        RCC_Wakeup();
-        // call the callback
-        sctimer_vars.sctimer_cb();
-        // kick the OS
-        return KICK_SCHEDULER;
-    }
-    return DO_NOT_KICK_SCHEDULER;
-}*/
